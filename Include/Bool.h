323bf667d404615d997d633f513733b8f3c6f337
krishpranav <krisna.pranav@gmail.com>
2023-07-17 15:25:08 +0530

    Include: Bool

--- Include/Bool.h
+++ Include/Bool.h
@@ -15,5 +15,52 @@
 
 namespace Include
 {
-    
+    struct Bool;
+
+    struct Bool
+    {
+        bool _val;
+
+        Bool() = delete;
+
+        /**
+         * @overload _val
+         */
+        constexpr Bool(LibMeta::Boolean auto value) : _val(value) {}
+
+        /**
+         * @param value 
+         * @return constexpr Bool& 
+         */
+        constexpr Bool &operator=(LibMeta::Boolean auto value)
+        {
+            _val = value;
+            return *this;
+        }
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        explicit constexpr operator bool() const
+        {
+            return _val;
+        }
+
+        constexpr Bool operator() const
+        {
+            return Bool(not _val);
+        }
+    };
+
+    /**
+     * @param a 
+     * @param b 
+     * @return true 
+     * @return false 
+     */
+    constexpr bool operator==(Bool a, Bool b) 
+    {
+        return static_cast<bool>(a) == static_cast<bool>(b);
+    }
 }
