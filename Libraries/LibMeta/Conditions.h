f2050f2b205c9411f56a5c03d98aeb82045033f6
krishpranav <krisna.pranav@gmail.com>
2023-07-17 17:52:12 +0530

    LibMeta: Conditions

---
+++ Libraries/LibMeta/Conditions.h
@@ -0,0 +1,31 @@
+/**
+ * @file Conditions.h
+ * @author Krisna Pranav
+ * @brief conditions
+ * @version 1.0
+ * @date 2023-07-17
+ * 
+ * @copyright Copyright (c) 2023 RiffOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+namespace LibMeta
+{
+    template<bool condition, typename TTrue, typename TFalse>
+    struct _Condition
+    {
+        using Type = TTrue;
+    };
+
+    template<typename TTrue, typename TFalse>
+    struct _Condition<false, TTrue, TFalse>
+    {
+        using Type = TFalse;
+    };
+
+    template <bool condition, typename TTrue, typename TFalse>
+    using Condition = typename _Condition<condition, TTrue, TFalse>::Type;
+
+}
