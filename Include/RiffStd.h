d561da5ecde6ee3ee8e41effb156180e9c930206
krishpranav <krisna.pranav@gmail.com>
2023-07-17 16:07:38 +0530

    Include: RiffStd

---
+++ Include/RiffStd.h
@@ -0,0 +1,37 @@
+/**
+ * @file RiffStd.h
+ * @author Krisna Pranav
+ * @brief RiffSTD
+ * @version 1.0
+ * @date 2023-07-17
+ * 
+ * @copyright Copyright (c) 2023 RiffOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+
+#include <initializer_list>
+#include <new>
+#include <utility>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+#include <coroutine>
+#include <Include/Bool.h>
+#include <Include/Types.h>
+#include <Include/Keywords.h>
+
+#ifndef NTP_LEVEL_USING
+
+namespace Include
+{
+}
+
+using namespace Include;
+
+#endif
