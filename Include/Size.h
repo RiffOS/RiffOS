ed6fb241f3dd6b604bf20ec9a47803f4cbcb6dc8
krishpranav <krisna.pranav@gmail.com>
2023-07-17 17:49:28 +0530

    Include: Size + Try

--- Include/Size.h
+++ Include/Size.h
@@ -13,41 +13,80 @@
 
 #include "RiffStd.h"
 
-namespace Include
+namespace Include 
 {
+
     /**
      * @param v 
      * @return constexpr usize 
      */
-    inline constexpr usize kib(usize v)
-    {
-        return v * 1024;
+    inline constexpr usize kib(usize v) 
+    { 
+        return v * 1024; 
     }
 
     /**
      * @param v 
      * @return constexpr usize 
      */
-    inline constexpr usize mib(usize v)
-    {
-        return kib(v * 1024);
+    inline constexpr usize mib(usize v) 
+    { 
+        return kib(v * 1024); 
     }
 
     /**
      * @param v 
      * @return constexpr usize 
      */
-    inline constexpr usize gib(usize v)
-    {
-        return mib(v * 1024);
+    inline constexpr usize gib(usize v) 
+    { 
+        return mib(v * 1024); 
     }
 
     /**
      * @param v 
      * @return constexpr usize 
      */
-    inline constexpr usize tib(usize v)
-    {
-        return gib(v * 1024);
+    inline constexpr usize tib(usize v) 
+    { 
+        return gib(v * 1024); 
     }
-}
+
+    // MARK :- convertion
+
+    /**
+     * @param v 
+     * @return constexpr usize 
+     */
+    inline constexpr usize toKib(usize v) 
+    { 
+        return v / 1024; 
+    }
+
+    /**
+     * @param v 
+     * @return constexpr usize 
+     */
+    inline constexpr usize toMib(usize v) 
+    { 
+        return toKib(v) / 1024; 
+    }
+
+    /**
+     * @param v 
+     * @return constexpr usize 
+     */
+    inline constexpr usize toGib(usize v) 
+    { 
+        return toMib(v) / 1024; 
+    }
+
+    /**
+     * @param v 
+     * @return constexpr usize 
+     */
+    inline constexpr usize toTib(usize v) 
+    { 
+        return toGib(v) / 1024; 
+    }
+} 
---
+++ Include/Try.h
@@ -0,0 +1,21 @@
+/**
+ * @file Try.h
+ * @author Krisna Pranav
+ * @brief TRy
+ * @version 1.0
+ * @date 2023-07-17
+ * 
+ * @copyright Copyright (c) 2023 RiffOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+// TODO: Need to implement try block for RiffOS
+
+#include "Bool.h"
+#include "RiffStd.h"
+
+namespace Include
+{
+}
