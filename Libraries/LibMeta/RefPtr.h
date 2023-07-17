6ca9c92150f46c00fa64a525f3e8645162de960c
krishpranav <krisna.pranav@gmail.com>
2023-07-17 18:03:58 +0530

    Libraries: LibMeta[RefPtr]

--- Libraries/LibMeta/RefPtr.h
+++ Libraries/LibMeta/RefPtr.h
@@ -14,34 +14,230 @@
 
 #include "Conditions.h"
 
-namespace LibMeta
+namespace LibMeta 
 {
+
+    /// Mark :- Constants
+
+    /**
+     * @tparam T 
+     */
     template <typename T>
-    inline constexpr bool _Const = false;
+    inline constexpr bool _CONST = false;
 
+    /**
+     * @tparam T 
+     */
     template <typename T>
-    inline constexpr bool _Const<T const> = true;
+    inline constexpr bool _CONST<T const> = true;
 
-    template<typename T>
-    concept Const = _Const<T>;
+    /**
+     * @tparam T 
+     */
+    template <typename T>
+    concept Const = _CONST<T>;
 
-    template<typename t>
-    struct _RemoveConst
+    /**
+     * @tparam T 
+     */
+    template <typename T>
+    struct _RemoveConst 
     {
         using Type = T;
     };
 
+    /**
+     * @tparam T 
+     */
     template <typename T>
-    struct _RemoveConst<T const>
+    struct _RemoveConst<T const> 
     {
         using Type = T;
     };
 
+    /**
+     * @tparam T 
+     */
     template <typename T>
     using RemoveConst = typename _RemoveConst<T>::Type;
 
-    template<typename Src, typename Dest>
-    using CopyConst = Condition<Const<Src>, Dest const, RemoveConst<Dest>>;
+    /**
+     * @tparam Src 
+     * @tparam Dest 
+     */
+    template <typename Src, typename Dest>
+    using CopyConst = Condition<Const<Src>, Dest const, RemoveConst<Dest> >;
+
+    /// MARK :- Volatile
+
+    /**
+     * @tparam T 
+     */
+    template <typename T>
+    inline constexpr bool _Volatile = false;
+
+    /**
+     * @tparam T 
+     */
+    template <typename T>
+    inline constexpr bool _Volatile<T volatile> = true;
+
+    /**
+     * @tparam T 
+     */
+    template <typename T>
+    concept Volatile = _Volatile<T>;
+
+    /**
+     * @tparam T 
+     */
+    template <typename T>
+    struct _RemoveVolatile 
+    {
+        using Type = T;
+    };
+
+    /**
+     * @tparam T 
+     */
+    template <typename T>
+    struct _RemoveVolatile<T volatile> {
+        using Type = T;
+    };
+
+    /**
+     * @tparam T 
+     */
+    template <typename T>
+    using RemoveVolatile = typename _RemoveVolatile<T>::Type;
+
+    /**
+     * @tparam Src 
+     * @tparam Dest 
+     */
+    template <typename Src, typename Dest>
+    using CopyVolatile = Condition<Volatile<Src>, Dest volatile, RemoveVolatile<Dest> >;
+
+    /**
+     * @tparam T 
+     */
+    template <typename T>
+    using RemoveConstVolatile = RemoveConst<RemoveVolatile<T> >;
 
 
-}
+    /// MARK :- Ref
+
+    /**
+     * @tparam ypename 
+     */
+    template <typename>
+    inline constexpr bool _LvalueRef = false;
+
+    /**
+     * @tparam T 
+     */
+    template <typename T>
+    inline constexpr bool _LvalueRef<T &> = true;
+
+    /**
+     * @tparam T 
+     */
+    template <typename T>
+    concept LvalueRef = _LvalueRef<T>;
+
+    /**
+     * @tparam ypename 
+     */
+    template <typename>
+    inline constexpr bool _RvalueRef = false;
+
+    /**
+     * @tparam T 
+     */
+    template <typename T>
+    inline constexpr bool _RvalueRef<T &&> = true;
+
+    /**
+     * @tparam T 
+     */
+    template <typename T>
+    concept RvalueRef = _RvalueRef<T>;
+
+    /**
+     * @tparam T 
+     */
+    template <typename T>
+    concept Ref = LvalueRef<T> or RvalueRef<T>;
+
+    /**
+     * @tparam T 
+     */
+    template <typename T>
+    struct _RemoveRef
+    {
+        using Type = T;
+    };
+
+    /**
+     * @tparam T 
+     */
+    template <typename T>
+    struct _RemoveRef<T &> 
+    {
+        using Type = T;
+    };
+
+    /**
+     * @tparam T 
+     */
+    template <typename T>
+    struct _RemoveRef<T &&> 
+    {
+        using Type = T;
+    };
+
+    /**
+     * @tparam T 
+     */
+    template <typename T>
+    using RemoveRef = typename _RemoveRef<T>::Type;
+
+    template <typename T>
+    using RemoveConstVolatileRef = RemoveConstVolatile<RemoveRef<T> >;
+
+    /// MARK :- Pointers
+
+    /**
+     * @tparam ypename 
+     */
+    template <typename>
+    inline constexpr bool _Ptr = false;
+
+    /**
+     * @tparam T 
+     */
+    template <typename T>
+    inline constexpr bool _Ptr<T *> = true;
+
+    template <typename T>
+    concept Ptr = _Ptr<T>;
+
+    /**
+     * @tparam T 
+     */
+    template <typename T>
+    struct _RemovePtr 
+    {
+        using Type = T;
+    };
+
+    template <typename T>
+    struct _RemovePtr<T *> 
+    {
+        using Type = T;
+    };
+
+    template <typename T>
+    using RemovePtr = typename _RemovePtr<RemoveConstVolatile<T> >::Type;
+
+} 
