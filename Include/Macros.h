870a97814045d1b5747503c4435c2e43c1bd4da3
krishpranav <krisna.pranav@gmail.com>
2023-07-17 18:55:39 +0530

    Include: Macros

--- Include/Macros.h
+++ Include/Macros.h
@@ -11,8 +11,22 @@
 
 #pragma once
 
-namespace Include
+namespace Include 
 {
+
+    #define goto _Pragma("GCC error \"goto is not allowed\"") goto
+
+    #define arrayLen$(ARR) (sizeof(ARR) / sizeof(ARR[0]))
+
+    #define __concat$(LHS, RHS) LHS##RHS
+
+    #define concat$(LHS, RHS) __concat$(LHS, RHS)
+
+    #define __stringify$(SYM) #SYM
+
+    #define stringify$(SYM) __stringify$(SYM)
+
+    #define var$(NAME) concat$(_MACRO_VAR_##NAME##__, __LINE__)
 
     #define ALWAYS_INLINE [[gnu::always_inline]]
 
@@ -24,7 +38,7 @@ namespace Include
     ALWAYS_INLINE static inline T unionCast(U value)
         requires(sizeof(T) == sizeof(U))
     {
-        union X
+        union X 
         {
             U u;
             T t;
