d5ecd461f7b939b08aba3e7ee700e836882c7c7a
krishpranav <krisna.pranav@gmail.com>
2023-07-24 19:24:43 +0530

    RiffMeta: ID

--- Libraries/RiffMeta/ID.h
+++ Libraries/RiffMeta/ID.h
@@ -17,5 +17,59 @@
 
 namespace RiffMeta
 {
-    
+    using Id = usize;
+
+    template <typename T>
+    static Id idOf()
+    {
+        return reinterpret_cast<Id>(__PRETTY_FUNCTION__ + 41);
+    };
+
+    template <typename T>
+    static char const *nameOf()
+    {
+        return __PRETTY_FUNCTION__ + 40;
+    }
+
+    template <typename T = struct __noType>
+    struct Type;
+
+    template <typename T>
+    struct Type
+    {
+        using TYPE = T;
+
+        constexpr static Id id() 
+        {
+            return idOf<T>();
+        }
+
+        constexpr static char const *name() 
+        {
+            return nameOf<T>();
+        }
+    };
+
+    template<>
+    struct Type<struct __noType>
+    {
+        Id _id;
+        char const *_name;
+
+        constexpr Id id() const
+        {
+            return _id;
+        }
+
+        constexpr char const *name() const
+        {
+            return _name;
+        }
+    };
+
+    template <typename T>
+    Type<T> typeOf()
+    {
+        return Type<T>{};
+    }
 }
