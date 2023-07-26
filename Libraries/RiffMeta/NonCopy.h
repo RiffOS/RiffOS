875620f5ef2687e16da32af1503d2f1983926276
krishpranav <krisna.pranav@gmail.com>
2023-07-26 21:52:03 +0530

    RiffMeta: NonCopy

---
+++ Libraries/RiffMeta/NonCopy.h
@@ -0,0 +1,35 @@
+/**
+ * @file noncopy.h
+ * @author Krisna Pranav
+ * @brief NonCopyable
+ * @version 1.0
+ * @date 2023-07-26
+ * 
+ * @copyright Copyright (c) 2023 RiffOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+namespace RiffMeta 
+{
+
+    /// @brief NonCopyable
+    struct NonCopy 
+    {
+        NonCopy() = default;
+        NonCopy(NonCopy const &) = delete;
+        NonCopy &operator=(NonCopy const &) = delete;
+        NonCopy(NonCopy &&) = default;
+        NonCopy &operator=(NonCopy &&) = default;
+    };
+
+    /// @brief Static
+    struct Static 
+    {
+        Static() = default;
+        Static(Static &&) = delete;
+        Static &operator=(Static &&) = delete;
+    };
+
+}
